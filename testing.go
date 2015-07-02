f785d808e861f8886bd0028e7647be183a69c3ac
kewei <kewei@tune.com>
2015-07-02 15:55:04 -0700

    this is for dip

--- testing.go
+++ testing.go
@@ -9,9 +9,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 }
 
+func hello1(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world1!")
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/1", hello1)
 	http.ListenAndServe(":8000", mux)
 
 }

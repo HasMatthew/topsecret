6e66e08d4c8b674cb867dbca57e55242b1e62b52
kewei <kewei@tune.com>
2015-07-08 15:09:35 -0700

    this is the update for error check and success code for post method

--- mini.go
+++ mini.go
@@ -59,6 +59,11 @@ type Click struct {
 	WindowsAid   string
 }
 
+type Validation struct {
+	Message    string
+	HttpStatus string
+}
+
 //the function which handle the get methods
 //get the json data form from database and server to browser
 func Geter(w http.ResponseWriter, r *http.Request) {
@@ -70,7 +75,7 @@ func Geter(w http.ResponseWriter, r *http.Request) {
 
 	//store the data from sql database to the temp struct
 	var c Click
-	err = row.Scan(&c.ID, &c.AdvertiserID, &c.SiteID, &c.IP, &c.IosIfa)
+	err := row.Scan(&c.ID, &c.AdvertiserID, &c.SiteID, &c.IP, &c.IosIfa)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -95,15 +100,24 @@ func Poster(w http.ResponseWriter, r *http.Request) {
 	//get the raw bytes of input data
 	bytes, errs := ioutil.ReadAll(r.Body)
 	if errs != nil {
-		fmt.Println(errs)
+		errString := fmt.Sprintf("buffer overflow %s", errs)
+		reponse(w, http.StatusBadRequest, errString)
 		return
 	}
 
 	//store the raw bytes to a temporary struct
 	var point Click
 	errs = json.Unmarshal(bytes, &point)
+	fmt.Println(errs)
 	if errs != nil {
-		fmt.Println(errs)
+		errString := fmt.Sprintf(" invalid Json format: %s", errs)
+		reponse(w, http.StatusBadRequest, errString)
+		return
+	}
+
+	//validate the ad-id and reponse corresponding http code and json error validation message
+	if point.AdvertiserID == 0 || point.SiteID == 0 {
+		reponse(w, http.StatusBadRequest, "your advertiserID or site ID may equals to 0")
 		return
 	}
 
@@ -116,10 +130,25 @@ func Poster(w http.ResponseWriter, r *http.Request) {
 		id, point.AdvertiserID, point.SiteID, ip, point.IosIfa)
 
 	if errs != nil {
-		fmt.Println(errs)
+		errString := fmt.Sprintf("bad insertion: %s", errs)
+		reponse(w, http.StatusBadRequest, errString)
 		return
+	}
+
+	//at the end of function the post data is success so output 200 code and the id for it
+	reponse(w, http.StatusOK, id)
+	return
+}
+
+func reponse(w http.ResponseWriter, status int, message string) {
+	w.WriteHeader(status)
+
+	validate := Validation{message, strconv.Itoa(status)}
+	bytes, errs := json.Marshal(&validate)
+	if errs != nil {
+		fmt.Println(errs) // this errors is only for execution no need to output to user
 	} else {
-		io.WriteString(w, id)
+		io.WriteString(w, string(bytes))
 	}
 
 }

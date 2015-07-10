4ea37088a0bbf42a3e3f5bb94aef4658c1faeaf7
kewei <kewei@tune.com>
2015-07-10 11:24:49 -0700

    change to the sql databases we used it now

--- logs.sql
+++ logs.sql
@@ -11,36 +11,3 @@ create table clicks
     windows_aid varchar(255) default NULL,
     created timestamp default current_timestamp
 );
-create table installs
-(
-    id varchar(255) NOT NULL,
-    advertiser_id int NOT NULL,
-    site_id int NOT NULL,
-    ip varchar(255) NOT NULL,
-    ios_ifa varchar(255) default NULL,
-    google_aid varchar(255) default NULL,
-    windows_aid varchar(255) default NULL,
-    stat_click_id varchar(255) default NULL,
-    created timestamp default current_timestamp
-);
-create table events
-(
-    id varchar(255) NOT NULL,
-    advertiser_id int NOT NULL,
-    site_id int NOT NULL,
-    ip varchar(255) NOT NULL,
-    ios_ifa varchar(255) default NULL,
-    google_aid varchar(255) default NULL,
-    windows_aid varchar(255) default NULL,
-    stat_install_id varchar(255) default NULL,
-    created timestamp default current_timestamp
-);
-create table persons
-(
-    first varchar(255) NOT NULL,
-    last varchar(255) NOT NULL,
-    age int NOT NULL,
-    major varchar(255) default NULL,
-    constellation varchar(255) default NULL,
-    created timestamp default current_timestamp
-);

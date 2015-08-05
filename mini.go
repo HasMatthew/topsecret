b83b5a7927aa15b947284d733c4e0ae88f75c7a9
Hasjoshp <joshp@tune.com>
2015-08-05 14:24:22 -0700

    updated logging to use new structured logging package

--- mini.go
+++ mini.go
@@ -7,41 +7,35 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"log"
 	"log/syslog"
-	mrand "math/rand"
+	"math/rand"
 	"net/http"
 	"strconv"
 	"time"
 
+	"github.com/MobileAppTracking/measurement/lib/structured"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 var (
-	logWriter *syslog.Writer
-	db        *sql.DB
+	db *sql.DB
 )
 
 func init() {
-	var err error
-	//set up the logwriter
-	logWriter, err = syslog.Dial("tcp", "localhost:10514", syslog.LOG_EMERG, "mini---project")
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	structured.AddHookToSyslog("tcp", "http://dp-joshp01-dev.sea1.office.priv:9200/database2/testdata", syslog.LOG_EMERG, "log Details: ")
 
 	//set up the database
-	db, err = sql.Open("mysql", "root@tcp(localhost:3306)/logs?parseTime=true")
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/logs?parseTime=true")
 	if err != nil {
-		logWriter.Err("can't open databases")
+		structured.Err("can't open databases")
 		return
 	}
 
 	// seed the random generator to generate IDs
-	mrand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())
 
 }
 
@@ -55,14 +49,13 @@ func main() {
 	server.ListenAndServe()
 
 	//close the database and log writer after the server stop running
-	logWriter.Close()
 	db.Close()
 }
 
 //build and return the server's handler
 func myHandler() *mux.Router {
 	mx := mux.NewRouter()
-	mx.HandleFunc("/", Poster).Methods("POST")
+	mx.HandleFunc("/", POST).Methods("POST")
 	mx.HandleFunc("/{id}", GET).Methods("GET")
 	return mx
 }
@@ -71,6 +64,7 @@ func myHandler() *mux.Router {
 
 type Click struct {
 	ID           string
+	Type         string
 	AdvertiserID int
 	SiteID       int
 	IP           string
@@ -106,44 +100,32 @@ func GET(writer http.ResponseWriter, reader *http.Request) {
 
 	//check for errors in scan  (404 and 500)
 	if err == sql.ErrNoRows {
-		fmt.Println(err)
+
+		// fmt.Println(err)
+
 		writer.WriteHeader(http.StatusNotFound)
-		io.WriteString(writer, `{"message" : "Error 404"}`)
-		io.WriteString(writer, `{"httpstatus" : "404"}`)
-		log.Print(`{"message" : "Error 404"}, `)
-		log.Println(`{"httpstatus" : "404"}`)
+
+		structured.Error(c.ID, c.Type, "Error 404: no rows found", c.SiteID, nil)
+
 		return
 	} else if err != nil {
+
 		writer.WriteHeader(http.StatusInternalServerError)
-		fmt.Println(err)
-		io.WriteString(writer, "Error 500")
-		log.Print(`{"message" : "Error 500"}, "`)
-		log.Println(`{"httpstatus" : "500"}`)
+
+		structured.Error(c.ID, c.Type, "Error 500: Internal server error", c.SiteID, nil)
+
 		return
 	}
-
-	//marshal the data from the temp struct to json
-	bytes, err := json.Marshal(&c)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	//output the raw bytes to the browser
-	writer.WriteHeader(http.StatusOK)
-	io.WriteString(writer, string(bytes))
 
 	// log the event
-	log.Println("GET:", c.ID)
-
-	//log the time
-	log.Println("GET: took", time.Now().Sub(starttime), "to execute")
+	structured.Info(c.ID, c.Type, "GET successful", c.SiteID,
+		structured.ExtraFields{"latency from request:": time.Since(starttime)})
 
 }
 
 //the function which handle the post method
 //post the json data from broswer to the server and sql databases
-func Poster(w http.ResponseWriter, r *http.Request) {
+func POST(w http.ResponseWriter, r *http.Request) {
 	//time when do request
 	RequestStart := time.Now()
 
@@ -161,7 +143,7 @@ func Poster(w http.ResponseWriter, r *http.Request) {
 	if errs != nil {
 		errString := fmt.Sprintf("invalid Json format: %s", errs)
 		response(w, errString, "", http.StatusBadRequest)
-		logWriter.Err(errString)
+		structured.Err(errString)
 		return
 	}
 
@@ -169,7 +151,7 @@ func Poster(w http.ResponseWriter, r *http.Request) {
 	if point.AdvertiserID == 0 || point.SiteID == 0 {
 		errString := "your advertiserID or site ID may equals to 0"
 		response(w, errString, "", http.StatusBadRequest)
-		logWriter.Err(errString)
+		structured.Err(errString)
 		return
 	}
 
@@ -188,21 +170,15 @@ func Poster(w http.ResponseWriter, r *http.Request) {
 		errString := "sorry, there is an error"
 		response(w, errString, "", http.StatusInternalServerError)
 		errString = fmt.Sprintf("database connection error : %s", errs)
-		logWriter.Err(errString)
+		structured.Err(errString)
 		return
 	}
 
-	responseTime("the time for inserting data to clicks table is ", QueryStart)
-
 	//sucess and log the request latency
 	response(w, "", id, http.StatusOK)
-	responseTime("the time for this Post request is ", RequestStart)
-}
-
-//report the query / request latency
-func responseTime(message string, startTime time.Time) {
-	responseDuration := time.Since(startTime)
-	logWriter.Info(message + responseDuration.String())
+	structured.Info(point.ID, point.Type, "Post successful!", point.SiteID,
+		structured.ExtraFields{"latency from request:": time.Since(RequestStart),
+			"latency from querry:": time.Since(QueryStart)})
 }
 
 //write the post reponse (faliure /success) to the client in Json format
@@ -216,7 +192,6 @@ func response(w http.ResponseWriter, errMessage string, id string, status int) {
 	} else {
 		w.Write(bytes)
 	}
-
 }
 
 //generate a random id to represent the unique id
@@ -234,7 +209,7 @@ func Hex(chunks int) string {
 
 	bytes := make([]byte, 4)
 	for i := 0; i < chunks; i++ {
-		binary.LittleEndian.PutUint32(bytes, mrand.Uint32())
+		binary.LittleEndian.PutUint32(bytes, rand.Uint32())
 		buffer.WriteString(hex.EncodeToString(bytes))
 	}
 
